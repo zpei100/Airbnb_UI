1785a6093f6f437008a7278d372f9803c3ba27b3
zpei100 <zhengqing.pei@gmail.com>
2019-01-03 17:15:34 -0500

    fixed route for calednar booking

--- src/client/actionCreators/updateDatesBooked.js
+++ src/client/actionCreators/updateDatesBooked.js
@@ -1,5 +1,4 @@
 import Axios from "axios";
-import host from '../serviceHost';
 
 export const UPDATE_DATES_BOOKED = 'UDPATE_DATES_BOOKED';
 
@@ -11,7 +10,7 @@ export default function({startDate, endDate, id, datesBooked}) {
   };
 
   return dispatch => {
-    Axios.post(`${host}/addDates`, {id, datesBooked}).then(({data: room}) => {
+    Axios.post('/addDates', {id, datesBooked}).then(({data: room}) => {
       alert('Since only this page is implemented. This alert box is your confirmation. Check your calendar again to see the dates have been booked!')
       dispatch({
         type: UPDATE_DATES_BOOKED,

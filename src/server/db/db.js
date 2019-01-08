ac2be0988a9743ca2e6bab6971dd3fbc66c643c9
zpei100 <zhengqing.pei@gmail.com>
2019-01-07 22:30:36 -0500

    pushign to heroku

--- src/server/db/db.js
+++ src/server/db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGOURL = process.env.MONGOURL || process.env.NODE_ENV === 'development' ? 'mongodb://localhost:27017/fec' : require('../config').MONGOURL;
+const MONGOURL = process.env.MONGOURL || require('../config').MONGOURL;
 
 console.log(MONGOURL)
 
